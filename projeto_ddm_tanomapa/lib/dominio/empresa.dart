8455cf9c25d4dbadfaaf5765e681384b1e5ac972
Gustavo Almeida <gustavo.marangoni.almeida@gmail.com>
2024-08-29 21:43:45 -0300

    atualizações feitas em 29/08

---
+++ projeto_ddm_tanomapa/lib/dominio/empresa.dart
@@ -0,0 +1,17 @@
+class Empresa {
+  late dynamic id;
+  late String nome;
+  late String tipo;
+  late String cidade;
+  late String estado;
+  late String ramo;
+  late String descricao;
+  late String hr_abertura;
+  late String hr_fechamento;
+  late String? horario;
+}
+
+horarioDeAtendimento(hr_fechamento, hr_abertura){
+  
+
+}
---
+++ projeto_ddm_tanomapa/lib/dominio/pages/login.dart
@@ -0,0 +1,19 @@
+
+//  if (!EmailCheck().isValidEmail(email.text)) {
+//       ScaffoldMessenger.of(context)
+//           .showSnackBar(SnackBar(content: Text("Email invalido")));
+//       return false;
+//     }
+//     return true;
+//   }
+//   void animateButton() {
+//     setState(() {
+//       _state = 1;
+//     });
+
+//     Timer(Duration(milliseconds: 3300), () {
+//       setState(() {
+//         _state = 2;
+//       });
+//     });
+//   }
